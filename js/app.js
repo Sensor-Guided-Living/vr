f069201209cef16b3c728bdca12d72c3898b851d
Wylky Ramirez <41131665+kingtoniormz@users.noreply.github.com>
2020-10-15 18:29:18 -0600

    cc

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 
 
+
 const http = require('http');
 
 const hostname = '127.0.0.1';
@@ -13,10 +14,7 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-});
-
-
-var APP = {
+});var APP = {
 
 	Player: function () {
 
