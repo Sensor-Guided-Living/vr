43ff29873c020038dfbe4baca5e827d73b9e889f
Wylky Ramirez <41131665+kingtoniormz@users.noreply.github.com>
2020-11-03 05:06:45 -0600

    cc

--- js/postprocessing/pago.js
+++ js/postprocessing/pago.js
@@ -3,7 +3,7 @@
 const stripe = require('stripe')('sk_test_51HjLb9ABZcByK9CS3uiY0nzR1NaquRLIeSozaYmj4tmKsneggbHEcX0eBS4sVdIqx9tEFoT23K1D6KHPvRGBIxbB00a8NlmzpH');
 
 const paymentIntent = await stripe.paymentIntents.create({
-  amount: 8,90,
+  amount: 8,
   currency: 'usd',
   payment_method_types: ['card'],
 });
